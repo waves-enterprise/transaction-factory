f951e038a29409ee7b0055beadfb8d2922eaa2a1
Ilya Zhavoronkov <izhavoronkov@web3tech.ru>
2022-02-25 16:02:47 +0300

    WE-7943 - Better wallet error handling

--- crypto/src/main/scala/com/wavesenterprise/crypto/WavesKeyStore.scala
+++ crypto/src/main/scala/com/wavesenterprise/crypto/WavesKeyStore.scala
@@ -1,5 +1,6 @@
 package com.wavesenterprise.crypto
 
+import cats.implicits.{catsStdInstancesForEither, catsSyntaxFlatMapOps}
 import cats.syntax.either._
 import com.wavesenterprise.account.Address
 import com.wavesenterprise.crypto.internals._
@@ -8,6 +9,7 @@ import com.wavesenterprise.utils.JsonFileStorage
 import play.api.libs.json.{Format, Json}
 
 import java.io.File
+import java.nio.file.Files
 import java.security.cert.Certificate
 import scala.collection.concurrent.TrieMap
 import scala.util.control.NonFatal
@@ -68,25 +70,21 @@ class WavesKeyStore(storageFolder: Option[File], password: Array[Char], chainId:
 
   private val key = JsonFileStorage.prepareKey(new String(password))
 
-  private def loadOrImport(f: File): Option[WalletFileData] =
-    try {
-      Some(JsonFileStorage.load[WalletFileData](f.getCanonicalPath, Some(key)))
-    } catch {
-      case NonFatal(_) => None
-    }
-
   private var walletData: WalletFileData = {
-    if (storageFolder.isEmpty)
-      WalletFileData(Seq.empty)
-    else {
-      val file = storageFolder.get
-      if (file.exists() && file.length() > 0) {
-        val wd = loadOrImport(file)
-        if (wd.isDefined) wd.get
-        else {
-          throw new IllegalStateException(s"Failed to open existing wallet file '${storageFolder.get}' maybe provided password is incorrect")
-        }
-      } else WalletFileData(Seq.empty)
+    storageFolder.fold {
+      throw new IllegalStateException("Wallet file is required for Waves crypto")
+    } { file =>
+      Either.cond(file.exists(), (), "File doesn't exist") >>
+        Either.cond(Files.isReadable(file.toPath), (), "File has no read access rights") >>
+        Either.cond(file.length() > 0, (), "File is empty") >>
+        (try {
+          JsonFileStorage.load[WalletFileData](file.getCanonicalPath, Some(key))
+        } catch {
+          case NonFatal(cause) => throw new RuntimeException(s"Unexpected error while reading wallet file '$file'", cause)
+        }) match {
+        case Right(data) => data
+        case Left(err)   => throw new IllegalStateException(s"Failed to read wallet file '$file': $err")
+      }
     }
   }
 
