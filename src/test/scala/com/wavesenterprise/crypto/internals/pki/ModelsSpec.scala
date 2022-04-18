f135287ee5bffbefb8a9d736465b206581fa7a6a
izhavoronkov <izhavoronkov@web3tech.ru>
2022-04-18 19:21:06 +0400

    WE-8003 - OIDs config fix

--- src/test/scala/com/wavesenterprise/crypto/internals/pki/ModelsSpec.scala
+++ src/test/scala/com/wavesenterprise/crypto/internals/pki/ModelsSpec.scala
@@ -1,8 +1,9 @@
 package com.wavesenterprise.crypto.internals.pki
 
-import com.wavesenterprise.crypto.internals.pki.Models.CertRequestContent
+import com.wavesenterprise.crypto.internals.pki.Models.{CertRequestContent, CustomExtendedKeyUsage, ExtendedKeyUsage}
 import org.scalatest.{FreeSpec, Matchers}
 import pureconfig.ConfigSource
+import pureconfig.error.ConfigReaderException
 
 class ModelsSpec extends FreeSpec with Matchers {
 
@@ -18,7 +19,7 @@ class ModelsSpec extends FreeSpec with Matchers {
         |        L = "Moscow"
         |        extensions {
         |            key-usage = ["digitalSignature"]
-        |            extended-key-usage = ["serverAuth"]
+        |            extended-key-usage = ["serverAuth", "1.2.3.4"]
         |            subject-alternative-name = "DNS:welocal.dev,DNS:localhost,IP:51.210.211.61,IP:127.0.0.1",
         |        }
         |    }
@@ -26,10 +27,31 @@ class ModelsSpec extends FreeSpec with Matchers {
 
     val conf = ConfigSource.string(confString).loadOrThrow[CertRequestContent]
 
-    assertResult(1)(conf.extensions.extendedKeyUsage.size)
-    assertResult(1)(conf.extensions.extendedKeyUsage.size)
-    assertResult(4)(conf.extensions.subjectAlternativeName.value.size)
+    conf.extensions.extendedKeyUsage should contain theSameElementsAs List(ExtendedKeyUsage.ServerAuth, CustomExtendedKeyUsage(Array(1, 2, 3, 4)))
+    conf.extensions.subjectAlternativeName.value.size shouldBe 4
+  }
 
+  "CertRequestContent should fail with empty mandatory fields" in {
+    val confString =
+      """
+        |   {
+        |        CN = ""
+        |        O = "Waves Enterprise"
+        |        OU = "IT Business"
+        |        C = "RU"
+        |        S = "Moscow"
+        |        L = "Moscow"
+        |        extensions {
+        |            key-usage = ["digitalSignature"]
+        |            extended-key-usage = ["serverAuth", "1.2.3.4"]
+        |            subject-alternative-name = "DNS:welocal.dev,DNS:localhost,IP:51.210.211.61,IP:127.0.0.1",
+        |        }
+        |    }
+        |""".stripMargin
+
+    (the[ConfigReaderException[_]] thrownBy ConfigSource
+      .string(confString)
+      .loadOrThrow[CertRequestContent]).getMessage should include("Mandatory fields 'CN', 'OU', 'O', 'C', 'L', 'S' must be non-empty")
   }
 
 }
