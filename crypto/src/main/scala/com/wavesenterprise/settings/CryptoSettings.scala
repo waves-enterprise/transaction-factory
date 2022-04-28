17663be26e029ab7361a1059bfc624be285591b9
kinebogin <kinebogin@gmail.com>
2022-04-28 19:36:54 +0400

    WE-8091 — CryptoSettings: Don't allow disabling CRL checks for pki.mode = ON

--- crypto/src/main/scala/com/wavesenterprise/settings/CryptoSettings.scala
+++ crypto/src/main/scala/com/wavesenterprise/settings/CryptoSettings.scala
@@ -98,8 +98,15 @@ object CryptoSettings extends ScorexLogging {
         case PkiMode.OFF => Right(DisabledPkiSettings)
         case PkiMode.ON =>
           for {
-            crlChecksEnabled <- parseCrlChecks(pkiCursor)
-            requiredOids     <- parseRequiredOIds(pkiCursor)
+            crlChecksEnabled <- parseCrlChecks(pkiCursor).flatMap {
+              case true =>
+                Right(true)
+              case false =>
+                Left(ConfigReaderFailures {
+                  ThrowableFailure(new IllegalStateException("Setting 'crl-checks-enabled = false' is forbidden for 'pki.mode = ON'"), None)
+                })
+            }
+            requiredOids <- parseRequiredOIds(pkiCursor)
           } yield EnabledPkiSettings(requiredOids, crlChecksEnabled)
         case PkiMode.TEST =>
           for {
