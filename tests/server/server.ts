c1bf1a5db15a2f0e31005d533b32e9817e652142
Artemiy Pospelov <apospelov@wavesenterprise.com>
2022-06-28 13:11:57 +0300

    WE-8140: modified TypeScriptTransactionsBytesSuite and server call isValid instead getSignatureBytes due not supported yet

--- tests/server/server.ts
+++ tests/server/server.ts
@@ -39,9 +39,8 @@ app.post('/', async (req, res) => {
     }
 
     const signatureGenerator = generator(tx);
-    const Uint8Bytes = await signatureGenerator.getSignatureBytes();
-    const Int8Bytes = Int8Array.from(Uint8Bytes)
-    return res.send({bytes: Array.from(Int8Bytes)})
+    const isValid = await signatureGenerator.isValid();
+    return res.send({isValid: isValid})
   } catch (err) {
     console.trace(err)
     res.send({error: err.message || err})
